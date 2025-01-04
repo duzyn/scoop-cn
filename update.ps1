77ee08019aa7e65e5ddb3c519843889d7b7e5ff7
David Peng <davidduzyn@outlook.com>
2025-01-05 06:25:57 +0800

    fix

--- update.ps1
+++ update.ps1
@@ -41,93 +41,93 @@ Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
     $content = Get-Content $_.FullName
 
     # GitHub Releases
-    $content = $content -replace "(https?://github\.com/.+/releases/.*download)", "$githubProxy/$1"
+    $content = $content -replace '(https?://github\.com/.+/releases/.*download)', '$githubProxy/$1'
 
     # GitHub Archive
-    $content = $content -replace "(https?://github\.com/.+/archive/)", "$githubProxy/$1"
+    $content = $content -replace '(https?://github\.com/.+/archive/)', '$githubProxy/$1'
 
     # GitHub Gists
-    $content = $content -replace "(https?://gist.github\.com/.+/)", "$githubProxy/$1"
+    $content = $content -replace '(https?://gist.github\.com/.+/)', '$githubProxy/$1'
 
     # GitHub Raw
-    $content = $content -replace "(https?://raw\.githubusercontent\.com)", "$githubProxy/$1"
-    $content = $content -replace "(https?://github\.com/.+/raw/)", "$githubProxy/$1"         
+    $content = $content -replace '(https?://raw\.githubusercontent\.com)', '$githubProxy/$1'
+    $content = $content -replace '(https?://github\.com/.+/raw/)', '$githubProxy/$1'         
 
     # DBeaver，not debaver-ea
-    $content = $content -replace "https?://dbeaver\.io/files/([\d\.]+)/", "$githubProxy/https://github.com/dbeaver/dbeaver/releases/download/$1/"
+    $content = $content -replace 'https?://dbeaver\.io/files/([\d\.]+)/', '$githubProxy/https://github.com/dbeaver/dbeaver/releases/download/$1/'
     
     # FastCopy
-    $content = $content -replace "https?://fastcopy\.jp/archive", "$githubProxy/https://raw.githubusercontent.com/FastCopyLab/FastCopyDist2/main"
+    $content = $content -replace 'https?://fastcopy\.jp/archive', '$githubProxy/https://raw.githubusercontent.com/FastCopyLab/FastCopyDist2/main'
 
     # OBS Studio
-    $content = $content -replace "https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows\.zip", "$githubProxy/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Windows.zip"
+    $content = $content -replace 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows\.zip', '$githubProxy/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Windows.zip'
 
     # OBS Studio 2.7
-    $content = $content -replace "https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Full", "$githubProxy/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Full"
+    $content = $content -replace 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Full', '$githubProxy/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Full'
 
     # Strawberry
-    $content = $content -replace "https?://files\.jkvinge\.net/packages/strawberry/StrawberrySetup-(.+)-mingw-x", "$githubProxy/https://github.com/strawberrymusicplayer/strawberry/releases/download/$1/StrawberrySetup-$1-mingw-x"
+    $content = $content -replace 'https?://files\.jkvinge\.net/packages/strawberry/StrawberrySetup-(.+)-mingw-x', '$githubProxy/https://github.com/strawberrymusicplayer/strawberry/releases/download/$1/StrawberrySetup-$1-mingw-x'
 
     # KDE Apps
-    # $content = $content -replace "download\.kde\.org", "mirrors.nju.edu.cn/kde"
+    # $content = $content -replace 'download\.kde\.org', 'mirrors.nju.edu.cn/kde'
 
     # 7-Zip
-    $content = $content -replace "www\.7-zip\.org/a", "mirrors.nju.edu.cn/7-zip"
+    $content = $content -replace 'www\.7-zip\.org/a', 'mirrors.nju.edu.cn/7-zip'
 
     # Blender
-    $content = $content -replace "download\.blender\.org", "mirrors.nju.edu.cn/blender"
+    $content = $content -replace 'download\.blender\.org', 'mirrors.nju.edu.cn/blender'
 
     # Cygwin
-    $content = $content -replace "//.*/cygwin/", "//mirrors.nju.edu.cn/cygwin/"
+    $content = $content -replace '//.*/cygwin/', '//mirrors.nju.edu.cn/cygwin/'
 
     # GIMP
-    $content = $content -replace "download\.gimp\.org/mirror/pub", "mirrors.nju.edu.cn/gimp"
+    $content = $content -replace 'download\.gimp\.org/mirror/pub', 'mirrors.nju.edu.cn/gimp'
     
     # Go
-    $content = $content -replace "dl\.google\.com/go", "mirrors.nju.edu.cn/golang"
+    $content = $content -replace 'dl\.google\.com/go', 'mirrors.nju.edu.cn/golang'
 
     # Gradle
-    $content = $content -replace "services\.gradle\.org/distributions", "mirror.nju.edu.cn/gradle"
+    $content = $content -replace 'services\.gradle\.org/distributions', 'mirror.nju.edu.cn/gradle'
 
     # Inkscape
-    $content = $content -replace "media\.inkscape\.org/dl/resources/file", "mirrors.nju.edu.cn/inkscape"
+    $content = $content -replace 'media\.inkscape\.org/dl/resources/file', 'mirrors.nju.edu.cn/inkscape'
 
     # Kodi
-    $content = $content -replace "mirrors\.kodi\.tv", "mirrors.nju.edu.cn/kodi"
+    $content = $content -replace 'mirrors\.kodi\.tv', 'mirrors.nju.edu.cn/kodi'
 
     # LaTeX, MiKTeX
-    $content = $content -replace "(miktex\.org/download/ctan)|(mirrors.+/CTAN)", "mirrors.nju.edu.cn/CTAN"
+    $content = $content -replace '(miktex\.org/download/ctan)|(mirrors.+/CTAN)', 'mirrors.nju.edu.cn/CTAN'
 
     # Node
-    $content = $content -replace "nodejs\.org/dist", "mirrors.nju.edu.cn/nodejs-release"
+    $content = $content -replace 'nodejs\.org/dist', 'mirrors.nju.edu.cn/nodejs-release'
     
     # Python
-    $content = $content -replace "www\.python\.org/ftp/python", "mirrors.nju.edu.cn/python"
+    $content = $content -replace 'www\.python\.org/ftp/python', 'mirrors.nju.edu.cn/python'
 
     # Vim
-    $content = $content -replace "ftp\.nluug\.nl/pub/vim/pc", "mirrors.nju.edu.cn/vim/pc"
+    $content = $content -replace 'ftp\.nluug\.nl/pub/vim/pc', 'mirrors.nju.edu.cn/vim/pc'
 
     # VirtualBox
-    $content = $content -replace "download\.virtualbox\.org/virtualbox", "mirrors.nju.edu.cn/virtualbox"
+    $content = $content -replace 'download\.virtualbox\.org/virtualbox', 'mirrors.nju.edu.cn/virtualbox'
 
     # VLC
-    $content = $content -replace "download\.videolan\.org/pub", "mirrors.nju.edu.cn/videolan-ftp"
+    $content = $content -replace 'download\.videolan\.org/pub', 'mirrors.nju.edu.cn/videolan-ftp'
 
     # Wireshark
-    $content = $content -replace "www\.wireshark\.org/download", "mirrors.nju.edu.cn/wireshark"
+    $content = $content -replace 'www\.wireshark\.org/download', 'mirrors.nju.edu.cn/wireshark'
 
     # Lunacy
-    $content = $content -replace "lun-eu\.icons8\.com/s/", "lcdn.icons8.com/"
+    $content = $content -replace 'lun-eu\.icons8\.com/s/', 'lcdn.icons8.com/'
 
     # Tor Browser, Tor
     # mirror list: https://raw.githubusercontent.com/torproject/torbrowser-launcher/refs/heads/main/share/torbrowser-launcher/mirrors.txt
-    $content = $content -replace "archive\.torproject\.org/tor-package-archive", "tor.calyxinstitute.org/dist"
+    $content = $content -replace 'archive\.torproject\.org/tor-package-archive', 'tor.calyxinstitute.org/dist'
 
     # Typora
-    $content = $content -replace "download\.typora\.io", "download2.typoraio.cn"
+    $content = $content -replace 'download\.typora\.io', 'download2.typoraio.cn'
 
     # Scripts
-    $content = $content -replace "(bucketsdir\\\\).+(\\\\scripts)", "$1scoop-cn$2"
+    $content = $content -replace '(bucketsdir\\\\).+(\\\\scripts)', '$1scoop-cn$2'
 
     # 将 suggest 路径改为 scoop-cn
     $content = $content -replace '\"main/|\"extras/|\"versions/|\"nirsoft/|\"sysinternals/|\"php/|\"nerd-fonts/|\"nonportable/|\"java/|\"games/', '"scoop-cn/' 
