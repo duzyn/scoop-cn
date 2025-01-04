f46ca03577e20673b7cbac52cc7aeb0b036eea22
David Peng <duzyn@users.noreply.github.com>
2025-01-05 00:11:39 +0800

    更新 update.ps1

--- update.ps1
+++ update.ps1
@@ -36,101 +36,99 @@ Remove-Item -Path .\scoop-games        -Recurse -Force
 Remove-Item -Path .\scoop-sysinternals -Recurse -Force
 
 Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
-    $content = Get-Content $_.FullName
+    $Content = Get-Content $_.FullName
 
     # GitHub Releases
-    $content = $content -replace '(github\.com/.+/releases/.*download)', 'ghgo.xyz/https://$1' 
+    $Content = $Content -replace '(https?://github\.com/.+/releases/.*download)', 'https://ghgo.xyz/$1' 
 
     # GitHub Archive
-    $content = $content -replace '(github\.com/.+/archive/)', 'ghgo.xyz/https://$1' 
+    $Content = $Content -replace '(https?://github\.com/.+/archive/)', 'https://ghgo.xyz/$1' 
 
     # GitHub Raw
-    $content = $content -replace '(raw\.githubusercontent\.com)', 'ghgo.xyz/https://$1' 
-    $content = $content -replace '(github\.com/.+/raw/)', 'ghgo.xyz/https://$1'          
+    $Content = $Content -replace '(https?://raw\.githubusercontent\.com)', 'https://ghgo.xyz/$1' 
+    $Content = $Content -replace '(https?://github\.com/.+/raw/)', 'https://ghgo.xyz/$1'          
 
     # KDE Apps
-    $content = $content -replace 'download\.kde\.org', 'mirrors.nju.edu.cn/kde' 
+    $Content = $Content -replace 'download\.kde\.org', 'mirrors.nju.edu.cn/kde' 
 
     # 7-Zip
-    $content = $content -replace 'www\.7-zip\.org/a', 'mirrors.nju.edu.cn/7-zip' 
+    $Content = $Content -replace 'www\.7-zip\.org/a', 'mirrors.nju.edu.cn/7-zip' 
 
     # LaTeX, MiKTeX
-    $content = $content -replace '(miktex\.org/download/ctan)|(mirrors.+/CTAN)', 'mirrors.nju.edu.cn/CTAN' 
+    $Content = $Content -replace '(miktex\.org/download/ctan)|(mirrors.+/CTAN)', 'mirrors.nju.edu.cn/CTAN' 
 
     # Node
-    $content = $content -replace 'nodejs\.org/dist', 'mirrors.nju.edu.cn/nodejs-release' 
+    $Content = $Content -replace 'nodejs\.org/dist', 'mirrors.nju.edu.cn/nodejs-release' 
     
     # Python
-    $content = $content -replace 'www\.python\.org/ftp/python', 'mirrors.nju.edu.cn/python' 
+    $Content = $Content -replace 'www\.python\.org/ftp/python', 'mirrors.nju.edu.cn/python' 
 
     # Go
-    $content = $content -replace 'dl\.google\.com/go', 'mirrors.nju.edu.cn/golang' 
+    $Content = $Content -replace 'dl\.google\.com/go', 'mirrors.nju.edu.cn/golang' 
 
     # VLC
-    $content = $content -replace 'download\.videolan\.org/pub', 'mirrors.nju.edu.cn/videolan-ftp' 
+    $Content = $Content -replace 'download\.videolan\.org/pub', 'mirrors.nju.edu.cn/videolan-ftp' 
 
     # Inkscape
-    $content = $content -replace 'media\.inkscape\.org/dl/resources/file', 'mirrors.nju.edu.cn/inkscape' 
+    $Content = $Content -replace 'media\.inkscape\.org/dl/resources/file', 'mirrors.nju.edu.cn/inkscape' 
 
     # DBeaver
-    $content = $content -replace 'dbeaver\.io/files', 'ghgo.xyz/https://github.com/dbeaver/dbeaver/releases/download' 
-    # $content = $content -replace 'dbeaver\.io/files', 'mirrors.nju.edu.cn/github-release/dbeaver/dbeaver' 
+    $Content = $Content -replace 'https?://dbeaver\.io/files', 'https://ghgo.xyz/https://github.com/dbeaver/dbeaver/releases/download'
 
     # OBS Studio
-    $content = $content -replace 'cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows\.zip', 'ghgo.xyz/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Windows.zip' 
-    # $content = $content -replace 'cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows\.zip', 'mirrors.nju.edu.cn/github-release/obsproject/obs-studio/OBS%20Studio%20$1/OBS-Studio-$1-Windows.zip' 
+    $Content = $Content -replace 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows\.zip', 'https://ghgo.xyz/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Windows.zip'
 
     # OBS Studio 2.7
-    $content = $content -replace 'cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Full', 'ghgo.xyz/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Full' 
+    $Content = $Content -replace 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Full', 'https://ghgo.xyz/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Full'
 
     # GIMP
-    $content = $content -replace 'download\.gimp\.org/mirror/pub', 'mirrors.nju.edu.cn/gimp' 
+    $Content = $Content -replace 'download\.gimp\.org/mirror/pub', 'mirrors.nju.edu.cn/gimp' 
 
     # Blender
-    $content = $content -replace 'download\.blender\.org', 'mirrors.nju.edu.cn/blender' 
+    $Content = $Content -replace 'download\.blender\.org', 'mirrors.nju.edu.cn/blender' 
 
     # VirtualBox
-    $content = $content -replace 'download\.virtualbox\.org/virtualbox', 'mirrors.nju.edu.cn/virtualbox' 
+    $Content = $Content -replace 'download\.virtualbox\.org/virtualbox', 'mirrors.nju.edu.cn/virtualbox' 
 
     # Wireshark
-    $content = $content -replace 'www\.wireshark\.org/download', 'mirrors.nju.edu.cn/wireshark' 
+    $Content = $Content -replace 'www\.wireshark\.org/download', 'mirrors.nju.edu.cn/wireshark' 
 
     # Lunacy
-    $content = $content -replace 'lun-eu\.icons8\.com/s/', 'lcdn.icons8.com/' 
+    $Content = $Content -replace 'lun-eu\.icons8\.com/s/', 'lcdn.icons8.com/' 
 
     # Strawberry
-    $content = $content -replace 'files\.jkvinge\.net/packages/strawberry/StrawberrySetup-(.+)-mingw-x', 'ghgo.xyz/https://github.com/strawberrymusicplayer/strawberry/releases/download/$1/StrawberrySetup-$1-mingw-x' 
+    $Content = $Content -replace 'https?://files\.jkvinge\.net/packages/strawberry/StrawberrySetup-(.+)-mingw-x', 'https://ghgo.xyz/https://github.com/strawberrymusicplayer/strawberry/releases/download/$1/StrawberrySetup-$1-mingw-x' 
 
     # Vim
-    $content = $content -replace 'ftp\.nluug\.nl/pub/vim/pc', 'mirrors.nju.edu.cn/vim/pc' 
+    $Content = $Content -replace 'ftp\.nluug\.nl/pub/vim/pc', 'mirrors.nju.edu.cn/vim/pc' 
 
     # Cygwin
-    $content = $content -replace '//.*/cygwin/', '//mirrors.nju.edu.cn/cygwin/' 
+    $Content = $Content -replace '//.*/cygwin/', '//mirrors.nju.edu.cn/cygwin/' 
 
     # Tor Browser, Tor
-    $content = $content -replace 'archive\.torproject\.org/tor-package-archive', 'tor.calyxinstitute.org/dist' 
+    $Content = $Content -replace 'archive\.torproject\.org/tor-package-archive', 'tor.calyxinstitute.org/dist' 
 
     # FastCopy
-    $content = $content -replace 'fastcopy\.jp/archive', 'ghgo.xyz/https://raw.githubusercontent.com/FastCopyLab/FastCopyDist2/main' 
+    $Content = $Content -replace 'https?://fastcopy\.jp/archive', 'https://ghgo.xyz/https://raw.githubusercontent.com/FastCopyLab/FastCopyDist2/main' 
 
     # Kodi
-    $content = $content -replace 'mirrors\.kodi\.tv', 'mirrors.nju.edu.cn/kodi' 
+    $Content = $Content -replace 'mirrors\.kodi\.tv', 'mirrors.nju.edu.cn/kodi' 
 
     # Typora
-    $content = $content -replace 'download\.typora\.io', 'download2.typoraio.cn' 
+    $Content = $Content -replace 'download\.typora\.io', 'download2.typoraio.cn' 
 
 		# Gradle
-		$content = $content -replace 'services\.gradle\.org/distributions', 'mirror.nju.edu.cn/gradle' 
+		$Content = $Content -replace 'services\.gradle\.org/distributions', 'mirror.nju.edu.cn/gradle' 
 
     # Scripts
-    $content = $content -replace '(bucketsdir\\\\).+(\\\\scripts)', '$1scoop-cn$2' 
+    $Content = $Content -replace '(bucketsdir\\\\).+(\\\\scripts)', '$1scoop-cn$2' 
 
     # 将 suggest 路径改为 scoop-cn
-    $content = $content -replace '\"main/|\"extras/|\"versions/|\"nirsoft/|\"sysinternals/|\"php/|\"nerd-fonts/|\"nonportable/|\"java/|\"games/', '"scoop-cn/' 
+    $Content = $Content -replace '\"main/|\"extras/|\"versions/|\"nirsoft/|\"sysinternals/|\"php/|\"nerd-fonts/|\"nonportable/|\"java/|\"games/', '"scoop-cn/' 
 
     # 将 depends 路径改为 scoop-cn
-    $content = $content -replace '\"depends\":\s*\"(scoop\-cn/)?', '"depends": "scoop-cn/' 
+    $Content = $Content -replace '\"depends\":\s*\"(scoop\-cn/)?', '"depends": "scoop-cn/' 
     
-		Set-Content -Path $_.FullName -Value $content
+		Set-Content -Path $_.FullName -Value $Content
 }
 
