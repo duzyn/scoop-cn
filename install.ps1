21f2d95339535b51a6ad81cb3a2932c91458671f
David Peng <davidduzyn@outlook.com>
2025-11-12 20:02:56 +0800

    Use gh-proxy.com

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@
 $global:ProgressPreference = 'SilentlyContinue'
 
 # 设置 GitHub 代理地址
-$GithubProxy = "https://gh-proxy.net"
+$GithubProxy = "https://gh-proxy.com"
 
 # 如果有设 SCOOP 环境变量，就按环境变量来
 $ScoopPath = if ($env:SCOOP) { $env:SCOOP } else { "$env:USERPROFILE\scoop" }
@@ -14,11 +14,11 @@ $ScoopPath = if ($env:SCOOP) { $env:SCOOP } else { "$env:USERPROFILE\scoop" }
 (Invoke-RestMethod "$GithubProxy/https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1").Replace("https://github.com", "$GithubProxy/https://github.com")  | Invoke-Expression
 
 # 将 Scoop 的仓库源替换为代理的
-scoop config scoop_repo "$GithubProxy/https://github.com/ScoopInstaller/Scoop"
+scoop config scoop_repo "$GithubProxy/https://github.com/ScoopInstaller/Scoop.git"
 
 # 替换 7zip, git 的下载地址为代理的
-(Get-Content "$ScoopPath\buckets\main\bucket\7zip.json") -replace 'https?://www\.7-zip\.org/a/7z(\d{2})(\d{2})', 'https://gh-proxy.net/https://github.com/ip7z/7zip/releases/download/$1.$2/7z$1$2' | Set-Content "$ScoopPath\buckets\main\bucket\7zip.json"
-(Get-Content "$ScoopPath\buckets\main\bucket\git.json") -replace '(https?://github\.com/.+/releases/.*download)', 'https://gh-proxy.net/$1' | Set-Content "$ScoopPath\buckets\main\bucket\git.json"
+(Get-Content "$ScoopPath\buckets\main\bucket\7zip.json") -replace 'https?://www\.7-zip\.org/a/7z(\d{2})(\d{2})', 'https://gh-proxy.com/https://github.com/ip7z/7zip/releases/download/$1.$2/7z$1$2' | Set-Content "$ScoopPath\buckets\main\bucket\7zip.json"
+(Get-Content "$ScoopPath\buckets\main\bucket\git.json") -replace '(https?://github\.com/.+/releases/.*download)', 'https://gh-proxy.com/$1' | Set-Content "$ScoopPath\buckets\main\bucket\git.json"
 
 # 安装时注意顺序是 7zip, git
 scoop install 7zip
@@ -26,10 +26,10 @@ scoop install git
 
 # 将 Scoop 的 main 仓库源替换为代理的
 if (Test-Path -Path "$ScoopPath\buckets\main") { scoop bucket rm main }
-scoop bucket add main "$GithubProxy/https://github.com/ScoopInstaller/Main"
+scoop bucket add main "$GithubProxy/https://github.com/ScoopInstaller/Main.git"
 
 # 添加 scoop-cn 仓库
-scoop bucket add scoop-cn "$GithubProxy/https://github.com/duzyn/scoop-cn"
+scoop bucket add scoop-cn "$GithubProxy/https://github.com/duzyn/scoop-cn.git"
 
 # 将 7zip 和 git 转为后续用 scoop-cn 来更新
 Get-ChildItem -Path "$ScoopPath\apps" -Recurse -Filter "install.json" | ForEach-Object { (Get-Content -Path $_.FullName -Raw) -replace '"bucket": "main"', '"bucket": "scoop-cn"' | Set-Content -Path $_.FullName }
